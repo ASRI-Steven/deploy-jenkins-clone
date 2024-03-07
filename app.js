2d52db2d182076659fa112b2eb71f11d67185069
Steven Alexander <2403077@alam-sutera.co.id>
2024-03-07 16:25:36 +0700

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+var cors = require('cors')
+var app = express()
+
+const port = 3000
+app.use(cors())
+
+app.get('/hello', (req, res) => {
+  res.send(process.env.HELLO || 'Hello')
+})
+
+app.get('/world', (req, res) => {
+  res.send(process.env.WORLD || 'World')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
