d0e58f21e9a4f59ca290175f00ffe90bf4183fcd
Steven <46786402+ASRI-Steven@users.noreply.github.com>
2024-03-12 13:16:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000
 app.use(cors())
 
 app.get('/hello', (req, res) => {
-  res.send(process.env.HELLO || '')
+  res.send(process.env.HELLO || 's')
 })
 
 app.get('/world', (req, res) => {
